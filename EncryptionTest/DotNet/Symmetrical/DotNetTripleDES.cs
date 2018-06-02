7e9e44730bbe09bb12c6d6d0af36647d7a9a24a9
slivka- <slivka104@gmail.com>
2018-06-02 11:45:15 +0200

    Added DES encryption

---
+++ EncryptionTest/DotNet/Symmetrical/DotNetTripleDES.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EncryptionTest.DotNet.Symmetrical
+{
+    class DotNetTripleDES
+    {
+        private static TripleDESCryptoServiceProvider TDESCrypto = null;
+        private static byte[] Key = null;
+        private static byte[] IV = null;
+
+        public static byte[] Encrypt(string input)
+        {
+            InitializeEncryption();
+            byte[] output = null;
+
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream cStream = new CryptoStream(mStream, TDESCrypto.CreateEncryptor(Key, IV), CryptoStreamMode.Write);
+
+            byte[] buffer = input.ToByteArray();
+            cStream.Write(buffer, 0, buffer.Length);
+            cStream.FlushFinalBlock();
+
+            output = mStream.ToArray();
+
+            cStream.Close();
+            mStream.Close();
+
+            return output;
+        }
+
+        public static string Decrypt(byte[] input)
+        {
+            InitializeEncryption();
+            string output = "";
+
+            MemoryStream mStream = new MemoryStream();
+            CryptoStream dStream = new CryptoStream(mStream, TDESCrypto.CreateDecryptor(Key, IV), CryptoStreamMode.Write);
+
+            dStream.Write(input, 0, input.Length);
+            dStream.FlushFinalBlock();
+
+            output = mStream.ToArray().ToStringUTF();
+
+            dStream.Close();
+            mStream.Close();
+
+            return output;
+        }
+
+        private static void InitializeEncryption()
+        {
+            if (TDESCrypto == null)
+                TDESCrypto = new TripleDESCryptoServiceProvider();
+            if (Key == null)
+            {
+                TDESCrypto.GenerateKey();
+                Key = TDESCrypto.Key;
+            }
+            if (IV == null)
+            {
+                TDESCrypto.GenerateIV();
+                IV = TDESCrypto.IV;
+            }
+        }
+    }
+}
--- EncryptionTest/Program.cs
+++ EncryptionTest/Program.cs
@@ -10,22 +10,29 @@ namespace EncryptionTest
             Console.WriteLine("Input message:");
             string inputString = Console.ReadLine();
 
-            Console.WriteLine();
-            byte[] DotNetEncrypted = DotNet.Symmetrical.DotNetAES.Encrypt(inputString);
-            Console.WriteLine(string.Format("Encrypted by .Net AES:\n{0}", DotNetEncrypted.ToStringUTF()));
+            Console.WriteLine("=============================SYMMETRICAL==================================================");
+            byte[] DotNetAESEncrypted = DotNet.Symmetrical.DotNetAES.Encrypt(inputString);
+            Console.WriteLine(string.Format("Encrypted by .Net AES:\n{0}", DotNetAESEncrypted.ToStringUTF()));
 
-            Console.WriteLine();
-            string DotNetDecrypted = DotNet.Symmetrical.DotNetAES.Decrypt(DotNetEncrypted);
-            Console.WriteLine(string.Format("Decrypted by .Net AES:\n{0}", DotNetDecrypted));
-            Console.WriteLine("==========================================================================================");
+            string DotNetAESDecrypted = DotNet.Symmetrical.DotNetAES.Decrypt(DotNetAESEncrypted);
+            Console.WriteLine(string.Format("Decrypted by .Net AES:\n{0}", DotNetAESDecrypted));
+            Console.WriteLine("------------------------------------------------------------------------------------------");
 
+            byte[] DotNetDESEncrypted = DotNet.Symmetrical.DotNetTripleDES.Encrypt(inputString);
+            Console.WriteLine(string.Format("Encrypted by .Net DES:\n{0}", DotNetDESEncrypted.ToStringUTF()));
+
+            string DotNetDESDecrypted = DotNet.Symmetrical.DotNetTripleDES.Decrypt(DotNetDESEncrypted);
+            Console.WriteLine(string.Format("Decrypted by .Net DES:\n{0}", DotNetDESDecrypted));
             Console.WriteLine();
+
+
+
+            Console.WriteLine("=============================HASHING======================================================");
             Console.WriteLine(string.Format("SHA265 by .Net:\n{0}", DotNet.Hashing.DotNetSHA2.GetHash(inputString)));
-            Console.WriteLine("==========================================================================================");
+            Console.WriteLine("------------------------------------------------------------------------------------------");
 
-            Console.WriteLine();
             Console.WriteLine(string.Format("MD5 by .Net:\n{0}", DotNet.Hashing.DotNetMD5.GetHash(inputString)));
-            Console.WriteLine("==========================================================================================");
+            Console.WriteLine();
 
             Console.ReadLine();
         }
