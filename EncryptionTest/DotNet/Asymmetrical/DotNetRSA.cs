fe448763d6333dec38797747d76e5d5bc538f4d5
slivka- <slivka104@gmail.com>
2018-06-02 11:59:03 +0200

    Added RSA Encryption

---
+++ EncryptionTest/DotNet/Asymmetrical/DotNetRSA.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+
+namespace EncryptionTest.DotNet.Asymmetrical
+{
+    class DotNetRSA
+    {
+        private static RSACryptoServiceProvider RSA = null;
+
+        public static byte[] Encrypt(string input)
+        {
+            InitializeEncryption();
+            return RSA.Encrypt(input.ToByteArray(), true);
+        }
+
+        public static string Decrypt(byte[] input)
+        {
+            InitializeEncryption();
+            return RSA.Decrypt(input, true).ToStringUTF(); ;
+        }
+
+        private static void InitializeEncryption()
+        {
+            if (RSA == null)
+                RSA = new RSACryptoServiceProvider();
+        }
+    }
+}
--- EncryptionTest/Program.cs
+++ EncryptionTest/Program.cs
@@ -25,7 +25,20 @@ namespace EncryptionTest
             Console.WriteLine(string.Format("Decrypted by .Net DES:\n{0}", DotNetDESDecrypted));
             Console.WriteLine();
 
+            Console.WriteLine("=============================ASYMMETRICAL=================================================");
+            byte[] DotNetRSAEncrypted = DotNet.Asymmetrical.DotNetRSA.Encrypt(inputString);
+            Console.WriteLine(string.Format("Encrypted by .Net RSA:\n{0}", DotNetRSAEncrypted.ToStringUTF()));
 
+            string DotNetRSADecrypted = DotNet.Asymmetrical.DotNetRSA.Decrypt(DotNetRSAEncrypted);
+            Console.WriteLine(string.Format("Decrypted by .Net RSA:\n{0}", DotNetRSADecrypted));
+            Console.WriteLine("------------------------------------------------------------------------------------------");
+
+            //byte[] DotNetDESEncrypted = DotNet.Symmetrical.DotNetTripleDES.Encrypt(inputString);
+            //Console.WriteLine(string.Format("Encrypted by .Net DES:\n{0}", DotNetDESEncrypted.ToStringUTF()));
+
+            //string DotNetDESDecrypted = DotNet.Symmetrical.DotNetTripleDES.Decrypt(DotNetDESEncrypted);
+            //Console.WriteLine(string.Format("Decrypted by .Net DES:\n{0}", DotNetDESDecrypted));
+            Console.WriteLine();
 
             Console.WriteLine("=============================HASHING======================================================");
             Console.WriteLine(string.Format("SHA265 by .Net:\n{0}", DotNet.Hashing.DotNetSHA2.GetHash(inputString)));
